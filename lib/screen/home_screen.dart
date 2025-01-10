69354953077a4fc3663152ce0bd5216593196c81
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-10 22:29:11 +0300

    packages added

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,44 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return KeyboardVisibilityBuilder(builder: (context, isKeyboardVisible) {
+      return Scaffold(
+        backgroundColor: Color(0xFFFFA500),
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          // Padding here
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Column(
+              children: [
+                // First Expanded for sharebible text and logo
+                // First Expanded for sharebible text and logo
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 60.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.share, color: Color(0xFFFFFFFF)),
+                        Text(
+                          'ShareBible',
+                          style: GoogleFonts.inter(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
