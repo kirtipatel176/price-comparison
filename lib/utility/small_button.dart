1970c52113b55cec5f61143c24a696c013a4bb85
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-11 17:20:03 +0300

    small button updated

--- lib/utility/small_button.dart
+++ lib/utility/small_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SmallButton extends StatefulWidget {
+class SmallButton extends StatelessWidget {
   const SmallButton({super.key, this.text, this.function, this.icon});
 
   final text;
@@ -8,12 +9,36 @@ class SmallButton extends StatefulWidget {
   final icon;
 
   @override
-  State<SmallButton> createState() => _SmallButtonState();
-}
-
-class _SmallButtonState extends State<SmallButton> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+      child: GestureDetector(
+        onTap: function,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5.0),
+          child: Container(
+            height: 56.0,
+            color: Color(0xFFFF6F61),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: Color(0xFFFFFFFF)),
+                SizedBox(
+                  width: 3.0,
+                ),
+                Text(
+                  text,
+                  style: GoogleFonts.rubik(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
