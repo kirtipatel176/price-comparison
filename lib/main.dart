2506e698f5826401dd5f6c16215aa4e38a82a533
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-11 21:16:23 +0300

    main upadated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tsena/screen/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
