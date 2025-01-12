9960b6bf0003c8cffe89739f9fbdd3f7cd204a0b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-12 19:53:48 +0300

    nodata updated

--- lib/screen/nodata_screen.dart
+++ lib/screen/nodata_screen.dart
@@ -5,6 +5,30 @@ class NodataScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Third Positioned widget for card (displaying details of single item)
+    return Positioned(
+      left: 0.0,
+      right: 0.0,
+      top: 260.0,
+
+      // Padding around container for displaying single item details
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+
+        // Container for displaying single item details
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: const Color(0xFFFFFFFF),
+            borderRadius: BorderRadius.circular(5.0),
+            border: Border.all(
+              color: const Color(0xFFD3D3D3),
+              width: 0.4,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
